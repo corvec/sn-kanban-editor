b497eb1208dc9dc9f4bfe81da3ff4a0773a5e312
Corey <755349+corvec@users.noreply.github.com>
2021-02-13 03:02:15 -0500

    Iterate on a unit test

--- src/lib/helpers.convertMarkdownToBoardData.test.ts
+++ src/lib/helpers.convertMarkdownToBoardData.test.ts
@@ -1,15 +1,19 @@
 import { convertMarkdownToBoardData } from './helpers';
+import { KanbanBoard } from '../../types/react-trello';
 
 test('converts simple markdown file', () => {
   const input = `# Lane 1
 * Card 1
   * Description: desc
   * Label: label`;
+  const expectedResult: KanbanBoard = {
+    lanes: [
+      {
+        title: 'Lane 1',
+        cards: [{ title: 'Card 1', description: 'desc', label: 'label' }],
+      },
+    ],
+  };
   const result = convertMarkdownToBoardData(input);
-  expect(result.lanes.length).toEqual(1);
-  expect(result.lanes[0].title).toEqual('Lane 1');
-  expect(result.lanes[0].cards.length).toEqual(1);
-  expect(result.lanes[0].cards[0].title).toEqual('Card 1');
-  expect(result.lanes[0].cards[0].description).toEqual('desc');
-  expect(result.lanes[0].cards[0].label).toEqual('label');
+  expect(result).toEqual(expectedResult);
 });
